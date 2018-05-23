8c8af5f2dacac6e2dce754b89a29efd5a1904b90
Sebastien Deger <deger.sebastien@gmail.com>
2018-05-23 14:52:46 +0200

    Update niv_arosage.ino

--- niv_arosage.ino
+++ niv_arosage.ino
@@ -23,9 +23,9 @@ void setup() {
   pinMode (flot_2, INPUT);
   pinMode (flot_3, INPUT);
 
-
+//defini l'étas des led (alumé ou éteinte )
   digitalWrite(led_bleu, LOW);
- digitalWrite(led_verte,LOW);
+  digitalWrite(led_verte,LOW);
   digitalWrite(led_orange, LOW);
   digitalWrite(led_rouge, LOW);
   digitalWrite(pompe, HIGH);
@@ -35,31 +35,31 @@ void setup() {
 
 void loop() {
  
-  if (digitalRead (flot_1)==LOW) //si le floteur niveau haut et pas activé 
+  if (digitalRead (flot_1)==LOW) //si le floteur niveau haut est pas activé 
   {
     digitalWrite(led_verte,LOW); // alors la led verte est eteinte
   }
-else 
-{
-digitalWrite(led_verte,HIGH); //sinon le led verte est allumé
-}
+      else 
+      {
+      digitalWrite(led_verte,HIGH); //sinon le led verte est allumé
+      }
 
 
 
 
- if (digitalRead (flot_2)==LOW)  //si le floteur niveau moyen est pas activé 
+  if (digitalRead (flot_2)==LOW)  //si le floteur niveau moyen est pas activé 
   {
-    digitalWrite(led_orange,LOW);// alors la led orange est eteinte
+    digitalWrite(led_orange,LOW);// alors la led orange est eteint
   }
-else 
-{
-digitalWrite(led_orange,HIGH); //sinon le led orange est allumé
-digitalWrite(led_bleu,LOW); // la led bleu s'etein quand la uuve et a moitier pleine ce qui signifie que on peut a nouveau utilisé la pompe
-digitalWrite(pompe,HIGH); // ferme le relay pour l'utilisation de la pompe 
-}
+      else 
+      {
+      digitalWrite(led_orange,HIGH); //sinon le led orange est allumé
+      digitalWrite(led_bleu,LOW); // la led bleu s'etein quand la uuve et a moitier pleine ce qui signifie que on peut a nouveau utilisé la pompe
+      digitalWrite(pompe,HIGH); // ferme le relay pour l'utilisation de la pompe 
+      }
 
 
- if (digitalRead (flot_3)==LOW) // si le floteur bas n'est pas activé on fait clignoté la led rouge " alerte niveau faible "
+  if (digitalRead (flot_3)==LOW) // si le floteur bas n'est pas activé on fait clignoté la led rouge " alerte niveau faible "
   {
     digitalWrite(led_rouge,LOW);
     delay(500);
@@ -68,15 +68,8 @@ digitalWrite(pompe,HIGH); // ferme le relay pour l'utilisation de la pompe
     digitalWrite(led_bleu,HIGH); // on allume la led bleu " indicateur de pompe non utilisable "
     digitalWrite(pompe,LOW); // et on coupe le relay de pompe  , on poura reutiliser la pompe que quand la cuve sera a moitier pleine 
   }
-else 
-{
-digitalWrite(led_rouge,HIGH); // sinon on allume juste la led rouge ui indique que la cuve est ntre le bas et la moitier
-
-
-}
-
-
-
-
-
+       else 
+       {
+        digitalWrite(led_rouge,HIGH); // sinon on allume juste la led rouge ui indique que la cuve est ntre le bas et la moitier
+       }
 }
